399fb489288a7e4faff97d3fea65926a33f3e147
el <95122504+elyasprba@users.noreply.github.com>
2024-08-12 18:09:41 +0700

    fixing (#11)
    Co-authored-by: elyasprba <elyasprba@Ts-MacBook-Pro.local>

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portofolio/', // Add only this section
   server: {
-    strictPort: true, // Ensures port usage strictness
-  },
-  build: {
-    target: 'modules', // or 'modules' if targeting older browsers
+    port: 4200,
   },
 });
