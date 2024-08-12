f29296d51b694fb195535fddc20acc1a4556364a
elyasprba <elyasprba@Ts-MacBook-Pro.local>
2024-08-12 17:44:24 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio/', // Add only this section
   server: {
     strictPort: true, // Ensures port usage strictness
   },
